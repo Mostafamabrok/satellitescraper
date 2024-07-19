e7145b60f49c342a5702fc7287b7b030f923aff2
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2024-07-19 10:55:33 +0300

    fixed dependacy

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setuptools.setup(
     long_description="PLACEHOLDER",
     long_description_content_type="text/markdown",
     url="https://github.com/Mostafamabrok/longitude",
-    install_requires=['certifi==2020.12.5','chardet==4.0.0','chromedriver-autoinstaller==0.2.2','idna==2.10',
-                      'numpy==1.19.5','requests==2.25.1','selenium==4.10.0','tqdm==4.53.0','urllib3==1.26.5', 
-                      'opencv-python==4.5.5.64'],
+    install_requires=['certifi','chardet','chromedriver-autoinstaller','idna',
+                      'numpy','requests','selenium','tqdm','urllib', 
+                      'opencv-python'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
